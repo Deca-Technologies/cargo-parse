cf0fbe3e016a594f8c67cd98e94c494c8cf029ec
Andrew Hoetker <andrew.hoetker@thinkdeca.com>
2021-10-21 15:41:31 -0600

    Implement unit testing and linting configs

--- examples/enforce_branch.py
+++ examples/enforce_branch.py
@@ -1,14 +1,16 @@
 """
 Example script to demonstrate the usefulness of `cargo-parse`.
 
-In this script, which could run as a GitHub Action, we assert that all Git dependencies in `Cargo.toml` are using the
-`master` branch. This check could run whenever a PR targets the `master` branch.
+In this script, which could run as a GitHub Action, we assert that all Git
+dependencies in `Cargo.toml` are using the `master` branch. This check could
+run whenever a PR targets the `master` branch.
 """
+
+from pathlib import Path
 
 from cargo_parse import parse_manifest_from_toml
 from cargo_parse.models.dependency import GitDependency
 
-from pathlib import Path
 
 PERMITTED_DEPENDENCY_BRANCHES = ["main", "master"]
 
@@ -19,4 +21,8 @@ if __name__ == "__main__":
         if isinstance(dependency, GitDependency):
             assert (
                 dependency.branch in PERMITTED_DEPENDENCY_BRANCHES
-            ), f"Branch '{dependency.branch}' of `{dependency.name}` not in {PERMITTED_DEPENDENCY_BRANCHES}"
+            ), "Branch '{}' of `{}` not in {}".format(
+                dependency.branch,
+                dependency.name,
+                PERMITTED_DEPENDENCY_BRANCHES,
+            )
